fc6aab5b66e81542c392f82cfa6f8a0136e10cd8
tris <tris.lett@gmail.com>
2019-10-03 10:07:37 +0200

    commenting for functions

--- ants_tbss/functions.py
+++ ants_tbss/functions.py
@@ -5,7 +5,24 @@ import numpy as np
 import argparse
 import nibabel as nib
 
+assert "ANTSPATH" in os.environ, "The environment variable ANTSPATH must be declared."
+ANTSPATH = os.environ['ANTSPATH']
+assert "FSLDIR" in os.environ, "The environment variable FSLDIR must be declared."
+FSLDIR = os.environ['FSLDIR']
+
 def get_wildcard(searchstring, printarray = False): # super dirty
+	"""
+	Essentially glob but using bash. It outputs search arrays if more than one file is found.
+	
+	Parameters
+	----------
+	searchstring : str
+	printarray : bool
+	
+	Returns
+	-------
+	outstring : str or array
+	"""
 	tmp_name = 'tmp_wildcard_%d' % np.random.randint(100000)
 	os.system('echo %s > %s' % (searchstring, tmp_name))
 	outstring = np.genfromtxt(tmp_name, dtype=str)
@@ -19,12 +36,40 @@ def get_wildcard(searchstring, printarray = False): # super dirty
 		return outstring
 
 def antsLinearRegCmd(numthreads, reference, mov, out_basename, outdir = None):
+	"""
+	Wrapper for ANTs linear registration with some recommended parameters.
+	Rigid transfomration: gradient step = 0.1
+	Mutual information metric: weight = 1; bins = 32.
+	Convergence: [1000x500x250x100,1e-6,10]
+	shrink-factors: 8x4x2x1
+	smoothing-sigmas: 3x2x1x0vox
+	
+	Parameters
+	----------
+	numthreads : int
+		The number of threads for parallel processing
+	reference : str
+		The reference image.
+	mov : str
+		The moving image.
+	out_basename : str
+		Output basename.
+	outdir : str
+		Output directory (options).
+
+	Returns
+	-------
+	ants_cmd : str
+		Output of the command (that can be piped to os.system).
+	"""
+
 	if not outdir:
 		outdir = ''
 	else:
 		if outdir[-1] != "/":
 			outdir = outdir + "/"
-	ants_cmd = ('export ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS=%d; antsRegistration -d 3 -r [ %s , %s, 1] -t Rigid[0.1] -m MI[ %s , %s , 1, 32] --convergence [1000x500x250x100,1e-6,10] --shrink-factors 8x4x2x1 --smoothing-sigmas 3x2x1x0vox -o [%s%s_, %s%s.nii.gz] --float' % (numthreads,
+	ants_cmd = ('export ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS=%d; %s/antsRegistration -d 3 -r [ %s , %s, 1] -t Rigid[0.1] -m MI[ %s , %s , 1, 32] --convergence [1000x500x250x100,1e-6,10] --shrink-factors 8x4x2x1 --smoothing-sigmas 3x2x1x0vox -o [%s%s_, %s%s.nii.gz] --float' % (numthreads,
+																														ANTSPATH,
 																														reference,
 																														mov,
 																														reference,
@@ -36,12 +81,41 @@ def antsLinearRegCmd(numthreads, reference, mov, out_basename, outdir = None):
 	return ants_cmd
 
 def antsNonLinearRegCmd(numthreads, reference, mov, out_basename, outdir = None):
+
+	"""
+	Wrapper for ANTs non-linear registration with some recommended parameters. I recommmend first using antsLinearRegCmd.
+	SyN transformation: [0.1,3,0] 
+	Mutual information metric: weight = 1; bins = 32.
+	Convergence: [100x70x50x20,1e-6,10]
+	shrink-factors: 8x4x2x1
+	smoothing-sigmas: 3x2x1x0vox
+	
+	Parameters
+	----------
+	numthreads : int
+		The number of threads for parallel processing
+	reference : str
+		The reference image.
+	mov : str
+		The moving image.
+	out_basename : str
+		Output basename.
+	outdir : str
+		Output directory (options).
+
+	Returns
+	-------
+	ants_cmd : str
+		Output of the command (that can be piped to os.system).
+	"""
+
 	if not outdir:
 		outdir = ''
 	else:
 		if outdir[-1] != "/":
 			outdir = outdir + "/"
-	ants_cmd = ('export ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS=%d; antsRegistration -d 3 --transform SyN[0.1,3,0] -m MI[ %s , %s , 1, 32] --convergence [100x70x50x20,1e-6,10] --shrink-factors 8x4x2x1 --smoothing-sigmas 3x2x1x0vox -o [%s%s_, %s%s.nii.gz]' % (numthreads,
+	ants_cmd = ('export ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS=%d; %s/antsRegistration -d 3 --transform SyN[0.1,3,0] -m MI[ %s , %s , 1, 32] --convergence [100x70x50x20,1e-6,10] --shrink-factors 8x4x2x1 --smoothing-sigmas 3x2x1x0vox -o [%s%s_, %s%s.nii.gz]' % (numthreads,
+																								ANTSPATH,
 																								reference,
 																								mov,
 																								outdir,
@@ -51,13 +125,37 @@ def antsNonLinearRegCmd(numthreads, reference, mov, out_basename, outdir = None)
 	return ants_cmd
 
 def antsApplyTransformCmd(reference, mov, warps, outname, outdir = None):
+
+	"""
+	Wrapper for applying ANTs transformations (warps).
+	
+	Parameters
+	----------
+	reference : str
+		The reference image.
+	mov : str
+		The moving image.
+	warps : arr
+		An array of warps to appy. It must always be an array even for a single warp!
+	outname : str
+		Output basename.
+	outdir : str
+		Output directory (options).
+
+	Returns
+	-------
+	ants_cmd : str
+		Output of the command (that can be piped to os.system).
+	"""
+
 	warps = np.array(warps)
 	if not outdir:
 		outdir = ''
 	else:
 		if outdir[-1] != "/":
 			outdir = outdir + "/"
-	ants_cmd = ('antsApplyTransforms -d 3 -r %s -i %s -e 0 -o %s%s --float' % (reference,
+	ants_cmd = ('%s/antsApplyTransforms -d 3 -r %s -i %s -e 0 -o %s%s --float' % (ANTSPATH,
+																										reference,
 																										mov,
 																										outdir,
 																										outname))
@@ -65,17 +163,37 @@ def antsApplyTransformCmd(reference, mov, warps, outname, outdir = None):
 		ants_cmd = ants_cmd + (' -t %s' % warps[i])
 	return ants_cmd
 
-def ants_bet(numthreads, reference_image, reference_image_brain_bn):
+def ants_bet(numthreads, input_image, output_image_brain):
+	"""
+	Wrapper for applying ANTs transformations (warps).
+	
+	Parameters
+	----------
+	numthreads : int
+		The number of threads for parallel processing
+	input_image : str
+		Anatomical image.
+	output_image_brain : str
+		Brain extracted output image.
+	
+	Returns
+	-------
+	ants_cmd : str
+		Output of the command (that can be piped to os.system).
+	"""
+
 	scriptwd = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-	T_template0 = "%s/static/ants_oasis_template_ras/T_template0.nii.gz" % scriptwd
-	T_template0_BrainCerebellumProbabilityMask = "%s/static/ants_oasis_template_ras/T_template0_BrainCerebellumProbabilityMask.nii.gz" % scriptwd
-	T_template0_BrainCerebellumRegistrationMask = "%s/static/ants_oasis_template_ras/T_template0_BrainCerebellumRegistrationMask.nii.gz" % scriptwd
-	os.system("export ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS=%d; antsBrainExtraction.sh -d 3 -a %s -e %s -m %s -f %s -o %s" % (numthreads, 
-																																									reference_image,
-																																									T_template0,
-																																									T_template0_BrainCerebellumProbabilityMask,
-																																									T_template0_BrainCerebellumRegistrationMask,
-																																									reference_image_brain_bn))
+	be_template = "%s/static/ants_oasis_template_ras/T_template0.nii.gz" % scriptwd
+	be_probability_mask = "%s/static/ants_oasis_template_ras/T_template0_BrainCerebellumProbabilityMask.nii.gz" % scriptwd
+	be_registration_mask = "%s/static/ants_oasis_template_ras/T_template0_BrainCerebellumRegistrationMask.nii.gz" % scriptwd
+	ants_cmd = ("export ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS=%d; %s/antsBrainExtraction.sh -d 3 -a %s -e %s -m %s -f %s -o %s" % (numthreads,
+																																									ANTSPATH, 
+																																									input_image,
+																																									be_template,
+																																									be_probability_mask,
+																																									be_registration_mask,
+																																									output_image_brain))
+	return ants_cmd
 
 
 
